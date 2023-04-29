bc8d7262bfc802715d4cdd5e678d2512db72fa0f
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-04-29 20:16:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,12 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+import Calculator from './Calculator';
 
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
